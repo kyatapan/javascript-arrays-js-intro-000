74f8feb01a0543d7941078f203ec9bb745a65ff9
kathrin v <go.kathrin@gmail.com>
2018-05-13 22:18:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,6 +31,9 @@ function removeElementFromBeginningOfArray(array) {
   return array.slice(1);
 }
 
-
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+}
 
 
