c9a647f3aeb4d427449bf50b1aa175984992d0c9
kathrin v <go.kathrin@gmail.com>
2018-05-13 22:15:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,10 +22,9 @@ function accessElementInArray(array, index) {
   return array[index];
 }
 
-
-
-
-
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift();
+}
 
 
 
