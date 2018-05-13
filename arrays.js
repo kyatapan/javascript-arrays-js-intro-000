ba69ec8e1f9ead0d8f2edd7c38ebebb537f0bf07
kathrin v <go.kathrin@gmail.com>
2018-05-13 22:07:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,4 +2,9 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
+  return array;
+}
+
+function addElementToBeginningOfArray(array, element) {
+  array = [element, ...array];
 }
